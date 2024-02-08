ae8e76d74082f2c3f44f3b26978a07c4711754f2
Noe <noe.satorres@gmail.com>
2024-02-08 15:13:20 -0300

    se creo componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componente/inicio/inicio.component';
+import { ContactoComponent } from './componente/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
