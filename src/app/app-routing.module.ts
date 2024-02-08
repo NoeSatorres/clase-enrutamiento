dfc3bb4ddc1fcf8bf43059f5bc0a04c6d8cd2b97
Noe <noe.satorres@gmail.com>
2024-02-08 15:22:01 -0300

    creacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componente/inicio/inicio.component';
+import { ContactoComponent } from './componente/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"inicio",component: InicioComponent},
+  {path: "contacto", component: ContactoComponent},
+  {path: "", redirectTo: "/inicio", pathMatch: "full"},
+  {path: "**", redirectTo: "/inicio", pathMatch: "full"},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
